f8769874511eae238bc9e3c591a78b905db6634b
Ping Lee <hackercollector4@gmail.com>
2018-08-11 04:36:12 +0800

    auth started

--- dev4u/settings.py
+++ dev4u/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'dev4u.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
