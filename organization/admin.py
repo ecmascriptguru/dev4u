cd10c0616a42da09ecc4855d95da1ede9d9a3f99
Ping Lee <hackercollector4@gmail.com>
2018-08-14 02:53:59 +0800

    team model created.

--- organization/admin.py
+++ organization/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, Team
+
+class TeamInline(admin.TabularInline):
+    """
+    Team admin model
+    """
+    model = Team
+    extra = 1
 
 class OrganizationAdmin(admin.ModelAdmin):
     """
     Organiation admin
     """
-    exclude=("created_at", "updated_at")
+    inlines = [TeamInline]
+    list_display = ('name', 'created_at', 'updated_at', 'how_many_teams')
 
 admin.site.register(Organization, OrganizationAdmin)
--- organization/models.py
+++ organization/models.py
@@ -8,9 +8,25 @@ class Organization(models.Model):
     Organization model
     """
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    created_at = models.DateTimeField('created at', default=timezone.now())
-    updated_at = models.DateTimeField('updated at', default=timezone.now())
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField('created at')
+    updated_at = models.DateTimeField('updated at')
 
     def __str__(self):
-        return self.name
+        return "<Organization({0}) : {1}>".format(self.id, self.name)
+    
+    def how_many_teams(self):
+        return len(self.team_set.all())
+
+class Team(models.Model):
+    """
+    Team model
+    """
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField('created at')
+    updated_at = models.DateTimeField('updated at')
+
+    def __str__(self):
+        return "<Team({0}) : {1}>".format(self.id, self.name)
